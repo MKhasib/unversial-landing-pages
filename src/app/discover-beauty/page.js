91ab393147770d36c48e978787436033c34b50e6
Majd Khasib <majd.khasib98@gmail.com>
2024-04-21 12:45:24 +0300

    create build and publish action

--- src/app/discover-beauty/page.js
+++ src/app/discover-beauty/page.js
@@ -1,14 +1,14 @@
 import Content from "./content";
 import Designs from "./designs";
 const DESIGNS = [
-	{ src: "/beauty-designs/first-design.png", alt: "first design" },
-	{ src: "/beauty-designs/second-design.png", alt: "second design" },
-	{ src: "/beauty-designs/third-design.png", alt: "third design" },
-	{ src: "/beauty-designs/fourth-design.png", alt: "fourth design" },
-	{ src: "/beauty-designs/fifth-design.png", alt: "fifth design" },
-	{ src: "/beauty-designs/sixth-design.png", alt: "sixth design" },
-	{ src: "/beauty-designs/seventh-design.png", alt: "seventh design" },
-	{ src: "/beauty-designs/eighth-design.png", alt: "eighth design" },
+	{ src: "/unversial-landing-pages/beauty-designs/first-design.png", alt: "first design" },
+	{ src: "/unversial-landing-pages/beauty-designs/second-design.png", alt: "second design" },
+	{ src: "/unversial-landing-pages/beauty-designs/third-design.png", alt: "third design" },
+	{ src: "/unversial-landing-pages/beauty-designs/fourth-design.png", alt: "fourth design" },
+	{ src: "/unversial-landing-pages/beauty-designs/fifth-design.png", alt: "fifth design" },
+	{ src: "/unversial-landing-pages/beauty-designs/sixth-design.png", alt: "sixth design" },
+	{ src: "/unversial-landing-pages/beauty-designs/seventh-design.png", alt: "seventh design" },
+	{ src: "/unversial-landing-pages/beauty-designs/eighth-design.png", alt: "eighth design" },
 ];
 export default function DiscoverBeauty() {
 	return (
